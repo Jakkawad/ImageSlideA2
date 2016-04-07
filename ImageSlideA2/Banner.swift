e6d1b9aa186fae1e4edf0eb8212a98301d6f6d2f
Jakkawad <admin@all2sale-imac.local>
2016-04-07 16:07:43 +0700

    basic

---
+++ ImageSlideA2/Banner.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+struct Banner {
+        static let initialImage = (
+        fileName: "1.jpg",
+        description: "The Great Auk drawing by John James Audubon, 1827-1838."
+    )
+    
+    static let localImages = [
+        (
+            fileName: "1.jpg",
+            description: "The Great Auks at Home, oil on canvas by John Gerrard Keulemans."
+        )
+    ]
+    
+    static let remoteImageBaseUrl = "http://sunny-juof.com/app/banner/"
+    
+    static let remoteImages = [
+        (
+            fileName: "1.png",
+            description: "Alca impennis by John Gould: The Birds of Europe, vol. 5 pl. 55, 19th century."
+        ),
+        (
+            fileName: "2.png",
+            description: "Alca impennis by John Gould: The Birds of Europe, vol. 5 pl. 55, 19th century."
+        ),
+        (
+            fileName: "3.png",
+            description: "Alca impennis by John Gould: The Birds of Europe, vol. 5 pl. 55, 19th century."
+        ),
+        (
+            fileName: "4.png",
+            description: "Alca impennis by John Gould: The Birds of Europe, vol. 5 pl. 55, 19th century."
+        )
+    ]
+}
--- ImageSlideA2/ViewController.swift
+++ ImageSlideA2/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import Auk
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //scrollView.auk.show(url: "http://sunny-juof.com/app/banner/1.png")
+        
+        for autoImage in Banner.remoteImages {
+            //let url = "\(Banner.remoteImageBaseUrl)\(remoteImages.fileName)"
+            let url = "\(Banner.remoteImageBaseUrl)\(autoImage.fileName)"
+            print(url)
+            scrollView.auk.show(url: url)
+            scrollView.auk.startAutoScroll(delaySeconds: 2)
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
